9510e4741fb9aeb1c520bb3b8a4aa239f9b3c802
Musab Hassan <musabhassan04@gmail.com>
2025-01-11 12:58:19 -0800

    Fix linting problems

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -48,8 +48,8 @@ exports.prayerTimesFetch = onRequest((request, response) => {
         const hanbaliJson = await islamicFinderRes.json();
         const hanbaliAsrTime = hanbaliJson.results.Asr;
 
-        asrHanbaliTime = new Date('1970-01-01T' + hanbaliAsrTime + 'Z').toLocaleTimeString('en-US',
-          { timeZone: 'PST', hour12: true, hour: '2-digit', minute: '2-digit' }
+        asrHanbaliTime = new Date("1970-01-01T" + hanbaliAsrTime + 'Z').toLocaleTimeString("en-US",
+          { timeZone: "PST", hour12: true, hour: "2-digit", minute: "2-digit" }
         );
       }
 
