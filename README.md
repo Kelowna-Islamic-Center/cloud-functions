342eb2237a99ff3ddb4b841470894171de2682d7
Musab Hassan <musab@musabhassan.com>
2025-10-10 20:38:28 -0700

    Detach from main - Update deployment for node 20

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
 <h2 align="center">
-  Firebase functions as backend
+  LEGACY BRANCH, This branch is only for the old apiFetch function incase it gets deleted and needs to be deployed again. This function will eventually be phased out but is currently still being used by a very small number of community memebers with old unsupported phones.
 </h2>
 <br>
 
-### There are currently two active functions
-
-## 1. ApiFetch
+## ApiFetch
 HTTPS function that fetches data from BCMA API and parses it.
 
-## 2. Announcements CMS Listener
-A Firestore listener that gets dispatches a CMS notification upon the addition of a new announcement.
-
-For functions to work, admin key must be provided as `./admin-key.json`. This key should never be shared publicly and should be added to `.gitignore`.
-
-### For the deployment of these functions to work, you must provide an "admin-key.json" file in the functions directory.
 </p>
--- functions/index.js
+++ functions/index.js
@@ -6,13 +6,8 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable max-len */
 
-const functions = require("firebase-functions");
+const functions = require("firebase-functions/v1");
 const fetch = require("node-fetch");
-const admin = require("firebase-admin");
-
-admin.initializeApp({
-  credential: admin.credential.cert(require("./admin-key.json"))
-});
 
 const cors = require("cors")({ origin: true });
 
@@ -55,34 +50,11 @@ exports.apiFetch = functions.https.onRequest((request, response) => {
   });
 });
 
-// Send cloud message upon adding a new announcement
-exports.announcementAlert = functions.firestore.document("/announcements/{documentId}").onCreate(async (snap, context) => {
-  const data = snap.data();
-
-  const payload = {
-    notification: {
-      title: `${data.title} - New Announcement`,
-      body: data.description,
-    }
-  };
-
-  // Set current server timeStamp to newly added announcement
-  await snap.ref.set({
-    timeStamp: admin.firestore.FieldValue.serverTimestamp()
-  }, { merge: true });
-
-  try {
-    await admin.messaging().sendToTopic("announcements", payload);
-  } catch (error) {
-    functions.logger.error("Failure sending notification", error);
-  }
-});
-
 
 function parseTime(time) {
   const parsedHour = (time.indexOf(":") == 1) ? `0${time.substring(0, time.indexOf(":"))}` : time.substring(0, time.indexOf(":"));
-  const parsedMinute = time.substring(time.indexOf(":")+1, time.indexOf(":")+3);
-  const aMpM = time.substring(time.length, time.length-2);
+  const parsedMinute = time.substring(time.indexOf(":") + 1, time.indexOf(":") + 3);
+  const aMpM = time.substring(time.length, time.length - 2);
 
   return `${parsedHour}:${parsedMinute} ${aMpM.toUpperCase()}`;
 }
